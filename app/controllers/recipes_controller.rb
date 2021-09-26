8c645bf7a03ae0088b664ffe43f48c050a52c7d0
Vandreh Esmeraldo <esmeraldovandreh@gmail.com>
2021-09-26 01:34:58 -0300

    Criando o form de edição

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,20 @@ class RecipesController < ApplicationController
 	def index
 		@recipes = Recipe.all
 	end
+
 	def show
 		@recipe = Recipe.find(params[:id])
 	end
-end
+
+	def edit
+		@recipe = Recipe.find(params[:id])
+	end
+
+	def update
+		#fail
+		@recipe = Recipe.find(params[:id])
+		recipe_params = params.require(:recipe).permit(:name, :stuff, :calories, :prepare_mode, :cost)
+		@recipe.update(recipe_params)
+		redirect_to @recipe
+	end
+end 
