de1994f9d04e99bbcfa90dbc2b02286944f6b357
Vandreh Esmeraldo <esmeraldovandreh@gmail.com>
2021-09-30 02:48:28 -0300

    Criando filtros customizados

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,10 @@
-class Recipe < ApplicationRecord
+class Recipe < ActiveRecord::Base
 	def light?
-		calories < 100
+		calories.blank? || calories < 100
 	end
+
+	def self.created_at
+		where('created_at <= ?', Time.now).order('created_at desc')
+	end
+
 end
