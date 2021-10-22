a2760f0b6e9b41811c2e953677c5353f3c964b45
Vandreh Esmeraldo <esmeraldovandreh@gmail.com>
2021-10-22 04:47:17 -0300

    Criando formulario de comentários

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+	#codigos do Fabricio Mattos
+	#protect_from_forgery with: :exception
+	#before_action :set_recipe, only:new except: destroy
 end
